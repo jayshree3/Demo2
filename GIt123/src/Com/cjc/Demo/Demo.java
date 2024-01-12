7ffae67a41e8cf3f582dc101fef7f74d834005f6
HP <HP@LAPTOP-KUH9GEJ5>
2024-01-12 17:15:49 +0530

    By Jayshree

---
+++ GIt123/src/Com/cjc/Demo/Demo.java
@@ -0,0 +1,15 @@
+package Com.cjc.Demo;
+
+public class Demo 
+{
+	public static void m1()
+	{
+		System.out.println("M1---A");
+	}
+	public static void main(String[] args) 
+	{
+		
+		Demo.m1();
+	}
+
+}
